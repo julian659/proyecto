ff0d7dc93104d68c22fa26e523c425e2fe5a2c5b
Julian <julian659@hotmail.com>
2022-08-11 20:07:50 -0500

    try 1

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(10, 20, 30);
+
+        Coche miCoche = new Coche();
+
+        miCoche.sumarPuertas();
+        miCoche.sumarPuertas();
+        System.out.println("El numero de puertas es " + miCoche.numeroDePuertas);
+
+    }
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+        System.out.println(resultado);
+    }
+}
+
+class Coche {
+    public int numeroDePuertas = 4;
+
+    public void sumarPuertas() {
+
+        this.numeroDePuertas++;
+
+    }
+}
